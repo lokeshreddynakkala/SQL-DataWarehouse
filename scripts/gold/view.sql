e07f248a1d87dcd374c36ae2d67c424aafb7e752
lokeshreddynakkala <lokesh67b2@gmail.com>
2025-10-22 20:40:01 +0530

    Create view.sql

---
+++ scripts/gold/view.sql
@@ -0,0 +1,67 @@
+use datawarehouse
+
+----creating dimension customers -----
+
+create view gold.dim_customers as
+select 
+row_number() over( order by cst_id) as customer_key,
+ci.cst_id                           as customer_id,
+ci.cst_key                          as customer_number,
+ci.cst_firstname                    as first_name ,
+ci.cst_lastname                     as last_name,
+ci.cst_marital_status               as marital_status,
+case
+when ci.cst_gndr <> 'n/a' then ci.cst_gndr
+else coalesce (ca.gen,'n/a')
+end                                 as gender,
+cst_create_date                     as birthdate,
+la.cntry                            as country
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid 
+left join silver.erp_loc_a101 la
+on 
+ci.cst_key = la.cid
+
+
+----creating dimension products -----
+
+create view gold.dim_products as
+select
+row_number() over(order by pn.prd_start_dt,pn.prd_key) as product_key,
+pn.prd_id             as product_id,
+pn.cat_id             as category_id,
+pn.prd_key            as product_number,
+pn.prd_nm             as product_name,
+pc.cat                as category,
+pc.subcat             as subcategory,
+pc.maintenance        as maintenance,
+pn.prd_cost           as cost,
+pn.prd_line           as product_line,
+pn.prd_start_dt       as start_date
+from silver.crm_prd_info pn 
+left join 
+silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where pn.prd_end_dt is null
+
+---creating fact table--------
+
+create view gold.fact_sales As
+
+select 
+sd.sls_ord_num    as order_number,
+pr.product_key    as product_key,
+cu.customer_key   as customer_key,
+sd.sls_order_dt   as order_date,
+sd.sls_ship_dt    as shipping_date,
+sd.sls_due_dt     as due_date ,
+sd.sls_sales      as sales_amount,
+sd.sls_quantity   as quantity,
+sd.sls_price      as price
+from 
+silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
